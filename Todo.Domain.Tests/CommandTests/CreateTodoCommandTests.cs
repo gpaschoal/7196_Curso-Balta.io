5988bb811349fe84aacf660c842a860c2e216e7f
Guilherme Paschoal <gabpaschoal@outlook.com>
2020-07-26 09:56:28 -0300

    Testes de Unidade

---
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Todo.Domain.Tests.CommandTests
+{
+  [TestClass]
+  public class CreateTodoCommandTests
+  {
+    [TestMethod]
+    public void DadoUmCommandInvalido()
+    {
+
+      Assert.Fail();
+    }
+
+    [TestMethod]
+    public void DadoUmCommandValido()
+    {
+
+      Assert.Equals();
+    }
+  }
+}
