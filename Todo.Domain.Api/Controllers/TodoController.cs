97020e5a745dd6e120c25041961f0c6c939309c3
Guilherme Paschoal <gabpaschoal@outlook.com>
2020-07-27 22:26:25 -0300

    Finalizando o Controller

--- Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Domain.Commands;
@@ -20,10 +21,97 @@ namespace Todo.Domain.Api.Controllers
       return repository.GetAll("gui");
     }
 
+    [Route("done")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllDone(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      return repository.GetAllDone("gui");
+    }
+
+    [Route("undone")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllUndone(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      return repository.GetAllUndone("gui");
+    }
+
+    [Route("done/today")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllDoneForToday(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      return repository.GetByPeriod("gui", DateTime.Now.Date, true);
+    }
+
+    [Route("undone/today")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllUndoneForToday(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      return repository.GetByPeriod("gui", DateTime.Now.Date, false);
+    }
+
+    [Route("done/tomorrow")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllDoneForTomorrow(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      return repository.GetByPeriod("gui", DateTime.Now.Date.AddDays(1), true);
+    }
+
+    [Route("undone/tomorrow")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllUndoneForTomorrow(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      return repository.GetByPeriod("gui", DateTime.Now.Date.AddDays(1), false);
+    }
+
     [Route("")]
     [HttpPost]
     public GenericCommandResult Create(
         [FromBody] CreateTodoCommand command,
+        [FromServices] TodoHandler handler
+    )
+    {
+      command.User = "Gui";
+      return (GenericCommandResult)handler.Handlers(command);
+    }
+
+    [Route("")]
+    [HttpPut]
+    public GenericCommandResult Update(
+        [FromBody] UpdateTodoCommand command,
+        [FromServices] TodoHandler handler
+    )
+    {
+      command.User = "Gui";
+      return (GenericCommandResult)handler.Handlers(command);
+    }
+
+    [Route("mark-as-done")]
+    [HttpPut]
+    public GenericCommandResult MarkAsDone(
+        [FromBody] MarkTodoAsDoneCommand command,
+        [FromServices] TodoHandler handler
+    )
+    {
+      command.User = "Gui";
+      return (GenericCommandResult)handler.Handlers(command);
+    }
+
+    [Route("mark-as-undone")]
+    [HttpPut]
+    public GenericCommandResult MarkAsUnDone(
+        [FromBody] MarkTodoAsUndoneCommand command,
         [FromServices] TodoHandler handler
     )
     {
